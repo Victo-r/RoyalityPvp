a2384c0f9751388b83682e24273e7fff59beb706
Victor <victor@morel.ovh>
2019-11-09 21:52:53 +0100

    main created

---
+++ src/fr/villomega/royalitypvp/RoyalityPvP.java
@@ -0,0 +1,15 @@
+package fr.villomega.royalitypvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RoyalityPvP extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
